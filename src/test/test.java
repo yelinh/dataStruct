26d1c257c6ea53f38f1764dfa932a57d80663794
yelinh <yelinhai07@163.com>
2019-05-29 16:46:29 +0800

    创建数据结构专用文件夹

---
+++ src/test/test.java
@@ -0,0 +1,14 @@
+package test;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class test {
+    @Test
+    public void test(){
+        List<String> list = new ArrayList<>();
+
+    }
+}
